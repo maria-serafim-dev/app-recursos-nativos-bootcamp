94ae2fc9846c3cd1dd0286a4cd2483c18a01b207
maria-serafim-dev <maria.serafim.dev@gmail.com>
2022-07-12 11:25:51 -0300

    Módulo : "Como desenvolver uma agenda"

---
+++ app/src/main/java/com/example/recursosnativosbootcamp/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.recursosnativosbootcamp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.CalendarContract
+import com.example.recursosnativosbootcamp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        onClickListenerCalendar()
+    }
+
+    private fun onClickListenerCalendar() {
+        binding.setEvent.setOnClickListener {
+            val intent = Intent(Intent.ACTION_INSERT)
+                .setData(CalendarContract.Events.CONTENT_URI)
+                .putExtra(CalendarContract.Events.TITLE, "Bootcamp everts")
+                .putExtra(CalendarContract.Events.EVENT_LOCATION, "on line")
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, System.currentTimeMillis())
+                .putExtra(
+                    CalendarContract.EXTRA_EVENT_END_TIME,
+                    System.currentTimeMillis() + (60 * 60 * 1000)
+                )
+
+            startActivity(intent)
+
+        }
+    }
+}
