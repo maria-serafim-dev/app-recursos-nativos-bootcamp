95e9d3382211385ec24464cbc925e5178c44cebc
maria-serafim-dev <maria.serafim.dev@gmail.com>
2022-07-12 14:29:26 -0300

    Módulo : "Recursos de biblioteca de fotos"

---
+++ app/src/main/java/com/example/recursosnativosbootcamp/PhotoActivity.kt
@@ -0,0 +1,75 @@
+package com.example.recursosnativosbootcamp
+
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.recursosnativosbootcamp.databinding.ActivityPhotoBinding
+
+class PhotoActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPhotoBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityPhotoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        onClickButtonPick()
+    }
+
+    private fun onClickButtonPick() {
+        binding.btnPick.setOnClickListener {
+            if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+                    val permission = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    requestPermissions(permission, PERMISSION_CODE)
+                }else{
+                    pickImageFromGalery()
+                }
+            }else{
+                pickImageFromGalery()
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            PERMISSION_CODE -> {
+                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    pickImageFromGalery()
+                }else{
+                    Toast.makeText(this, "Permissão negada", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun pickImageFromGalery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+            binding.ivImageUser.setImageURI(data?.data)
+        }
+    }
+
+    companion object{
+        private const val PERMISSION_CODE = 1000
+        private const val IMAGE_PICK_CODE = 1001
+    }
+
+}
